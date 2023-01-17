91af00f0d4015fcb1c26a3d5ee86ca12f1e5672a
uohman <ulrikalovisa@gmail.com>
2023-01-17 10:47:15 +0200

    Handler to register button to make it work

--- frontend/src/components/Wrapper.js
+++ frontend/src/components/Wrapper.js
@@ -1,15 +1,19 @@
 import React from 'react';
-// import Registration from './Registration'
+import { useNavigate } from 'react-router-dom';
 import Login from './Login'
 
 const Wrapper = () => {
+
+  const navigate = useNavigate();
+  const handleClick = () => navigate('/register');
+
   return (
     <div className='Outer-wrapper'>
       <div className='Inner-wrapper'>
         <h1>Welcome to the secret page of super-duper secrets</h1>
         <p>Please log in to your account</p>
         <Login />
-        <p>Don't have an account?</p><a href="https://beautiful-selkie-55d887.netlify.app/register"><p>Register</p></a>
+        <p>Don't have an account?</p><button type="button" class="button" onClick={handleClick}>Register</button>
       </div>
     </div>
   );
